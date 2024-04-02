6c1476989bd90739dcb6b1c9c2de8f21837d0459
Ikwuegbu Michael <ikwuegbu300@gmail.com>
2024-04-02 17:22:44 +0100

    Update: add installed apps

--- ajo_circles/ajo_circles/settings.py
+++ ajo_circles/ajo_circles/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'users',
+    'wallets',
+    'circles',
+    'notifications',
+    'transactions',
+    
 ]
 
 MIDDLEWARE = [
